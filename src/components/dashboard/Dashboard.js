410adfc93d6c6ff2f2c930ffa2354a5941a39f5f
Khoa Huynh <khoawin92@gmail.com>
2018-09-22 15:16:23 -0700

    working good for single array

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,14 +1,15 @@
-
+// prefilled data option also
 import React, { Component, Fragment } from 'react';
 import Select from '@material-ui/core/Select';
 import { connect } from 'react-redux';
 import { withStyles } from '@material-ui/core/styles';
-import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
 
 import randomize from 'imeanireallycantbelieveitsnotrandom';
 
@@ -24,25 +25,80 @@ const styles = theme => ({
   selectEmpty: {
     marginTop: theme.spacing.unit * 2,
   },
+  card: {
+    marginTop: 10,
+    paddingLeft: 20,
+    width: 450,
+    display: 'inline-block',
+    marginLeft: 20,
+    float: 'left',
+  },
+  multiline: {
+    marginLeft: 10,
+  },
+  resultsDiv: {
+    margin: 30
+  }
 });
 
 
 
 class Dashboard extends Component {
-  
-  state = { 
+
+  state = {
     arrayType: '',
-    multiline: '',
-  }
+    singleArray: '',
+    singleResults: '',
+    singleIndex: '',
+    submitted: false,
+    endResult: '',
+  };
 
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
 
-  onSubmit = (e) => {
+  singleSubmit = (e) => {
     e.preventDefault();
-    let arr = this.state.multiline.split('\n')
-    console.log(arr);
+    this.setState({submitted: true});
+
+    
+    let endResult = randomize({
+      type: 'single',
+      array: this.state.singleArray.split('\n').map(element => {
+        try{
+          return JSON.parse(element);
+        } catch(err) {
+          return element
+        }
+      }),
+      index: this.convertToObject(this.state.singleIndex.split('\n')),
+      results: this.convertToObject(this.state.singleResults.split('\n')),
+    })
+
+    this.setState({endResult}, () => {console.log(this.state.endResult)})
+  };
+
+  convertToObject = (array) => {
+    let obj = {};
+    for (let i = 0; i < array.length; i++) {
+      let key = array[i].replace(/\s/g, ''
+      ).split(':')[0]
+
+      let value = array[i].replace(/\s/g, ''
+      ).split(':')[1]
+
+      try {
+        value = JSON.parse(array[i].replace(/\s/g, ''
+        ).split(':')[1])
+      } catch (err) {
+        //do nothing
+      }
+
+      obj[key] = value
+    }
+
+    return obj
   }
 
   render() {
@@ -68,26 +124,111 @@ class Dashboard extends Component {
 
             </Select>
           </FormControl>
-          {this.state.arrayType === 'single' && <TextField
-          
-          id="filled-multiline-flexible"
-          name="multiline"
-          label="Multiline"
-          multiline
-          rowsMax="4"
-          value={this.state.multiline}
-          onChange={this.handleChange}
-          margin="normal"
-          helperText="enter an element, and then hit enter"
-          variant="filled"
-        />}
+
+          {this.state.arrayType === 'single' &&
+            <TextField
+              className={classes.multiline}
+              id="filled-multiline-flexible"
+              name="singleArray"
+              label="Array Content"
+              multiline
+              rowsMax="4"
+              value={this.state.singleArray}
+              onChange={this.handleChange}
+              margin="normal"
+              helperText="enter an element, and then hit enter"
+              variant="filled"
+            />
+          }
+
+          {this.state.arrayType === 'single' &&
+            <TextField
+              className={classes.multiline}
+              id="filled-multiline-flexible"
+              name="singleIndex"
+              label="Index"
+              multiline
+              rowsMax="4"
+              value={this.state.singleIndex}
+              onChange={this.handleChange}
+              margin="normal"
+              helperText="a: [<start Index>, <end Index>]"
+              variant="filled"
+              placeholder="a: [0,4]"
+            />
+          }
+
+          {this.state.arrayType === 'single' &&
+            <TextField
+              className={classes.multiline}
+              id="filled-multiline-flexible"
+              name="singleResults"
+              label="Index"
+              multiline
+              rowsMax="4"
+              value={this.state.singleResults}
+              onChange={this.handleChange}
+              margin="normal"
+              helperText="a: <item count>"
+              variant="filled"
+              placeholder="a: 2"
+            />
+          }
+
         </form>
         {this.state.arrayType === 'multi' && <p>multi</p>}
-        {this.state.arrayType === 'single' && <p>single</p>}
         <br />
-        <Button onClick={this.onSubmit} size='small' variant="contained" color="primary" className={classes.button}>
+        <Button onClick={this.singleSubmit} size='small' variant="contained" color="primary" className={classes.button}>
           Submit
       </Button>
+        <br />
+        <br />
+        <div>
+          {this.state.arrayType === 'single' && <Card className={classes.card}>
+            <Typography color="textSecondary">input</Typography>
+            <pre>{`
+        import randomize from 'weighted-randomizer';
+
+        
+        randomize({
+            type: 'single',
+            array: [${this.state.singleArray.split('\n').map(element => {
+                try {
+                  JSON.parse(element)
+                  return element
+                } catch (err) {
+                  return '\'' + element + '\'';
+                }
+              })}],
+            index: {
+                ${this.state.singleIndex.split('\n').map((index, i) => {
+                if (i > 0) return `\n                ` + index
+                return index
+              })}
+            },
+            results: {
+                ${this.state.singleResults.split('\n').map((index, i) => {
+                if (i > 0) return `\n                ` + index
+                return index
+              })}
+            }
+        })
+        `}</pre>
+          </Card>}
+          {this.state.submitted && this.state.arrayType === 'single' && <Card className={classes.card}>
+            <Typography color="textSecondary">output</Typography>
+            <div className={classes.resultsDiv}>
+              {`[${this.state.endResult.map(element => {
+                try {
+                  JSON.parse(element)
+                  return element
+                } catch (err) {
+                  return '\'' + element + '\'';
+                }
+              })}]`}
+            </div>
+          </Card>}
+        </div>
       </Fragment>
     );
   }
